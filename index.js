ff03ebd7f46d2b25855d4d702b91e6297d89eb33
Zach <53745970+zach7815@users.noreply.github.com>
2022-07-27 15:52:34 +0100

    Changing approach to building app
    Using other projects as rough guides to help complete the project.

--- index.js
+++ index.js
@@ -1,66 +1,112 @@
 
 const buttons =document.querySelectorAll(".button");
-const prevOperand= document.querySelector(".previous-operand");
-const currentOperand=document.querySelector(".current-operand");
 const clear =document.querySelector(".clear");
-let currentInput=[];
-let operatorIndexes=[];
-let currentSymbolIndex;
-let calStarted=false;
-
 const numbers=document.querySelectorAll(".number");
 
-numbers.forEach(button=>button.addEventListener("click", (event)=>{
-  currentOperand.innerText+=event.target.value;
-}));
+// https://codepen.io/koselige/pen/XWZLLzv
+// https://codepen.io/wesleybertipaglia/pen/JjLPyNR
 
-buttons.forEach(button=> button.addEventListener("click", (event)=>{
-  const numbersReg=/[0-9]/gi;
-  const symbolsReg= /[\*|\-|+|\/]/;
-  const equalSymbol=/=/;
-  currentValue= event.target.value;
-  if(!currentValue.match(equalSymbol)&&!currentValue.match(symbolsReg))
-    {
-      
-      currentInput.push(currentValue);
-      console.log(currentInput);
-    }
-  else if (currentValue.match(symbolsReg)){
-      if(currentInput.length===0) 
-        return;
-    else{
-      currentInput=[currentInput.reduce((ac,cv)=>ac+String(cv))];
-      currentInput.push(currentValue);
-    //  prevOperand.innerText=topOperand;
-
-     console.log(currentInput)
-   
-    }
-    
-  }
-  
-
-  
-  
-}));
-
-
+const defaultState = {
+displayDigit:"0", 
+operator:null,
+operatorSelected:false,
+firstDigit:"", 
+secondDigit:"", 
+}
 
 const clearDisplay=()=>{
   prevOperand.innerText="";
 currentOperand.innerText="0";
-  currentInput=[];
-  console.log(currentInput);
 };
+
+
+
+buttons.forEach(button=> button.addEventListener("click", (event)=>{
+  currentValue= event.target.innerText;
+  let symbolRegex= /[\+|\-|\*|\/]/
+if(defaultState.firstDigit===""||defaultState.operator===null||defaultState.secondDigit===""){
+  handleCurrentValue(currentValue, symbolRegex)
+}
+
+
+
+
+
+
+
+  
+    
+    
+
+  
+  
+  
+}));
+
+const updateeDisplay = ()=>{
+ let  currentOperand=document.querySelector(".current-operand");
+ currentOperand.innerText=defaultState.displayDigit;
+};
+
+const inverseNumber= (displayNumber)=>{
+  if (displayNumber===0){
+    return
+  }
+  else{
+    displayNumber*-1;
+  }
+};
+
+
+const handleCurrentValue=(currentValue, symbolRegex)=>{ 
+  if(currentValue.match(symbolRegex)&&defaultState.firstDigit===null){
+    return;
+  }
+  else if (defaultState.operator===null&&!currentValue.match(symbolRegex)){
+    defaultState.firstDigit+=String(currentValue)
+    console.log(defaultState.firstDigit)
+  }
+  else if(defaultState.firstDigit!==""&& currentValue.match(symbolRegex)){
+    defaultState.operator=currentValue;
+    console.log(defaultState.operator);
+    console.log(defaultState);
+  }
+  else if( defaultState.firstDigit!==""&&defaultState.operator!==null){
+    defaultState.secondDigit+=String(currentValue);
+    console.log(defaultState);
+  }
+
+}
+
+
+
+
+
+
+const performCalculation=(num1, operator, num2)=>{
+  let result=null;
+switch (operator){
+  case "+":
+    result= num1+num2;
+    break
+    case "-":
+      result=num1-num2;
+      break
+      case "/":
+        result=num1/num2;
+        break
+        case "*": 
+       result= num1*num2;
+        break
+        case "%":
+        result=num1/10
+        break
+}
+
+return result;
+}
 
 clear.addEventListener("click", clearDisplay);
 
-const toggleNegative =()=>{
-  if (currentOperand[0] !== "-"){
-console.log(currentOperand[0]);
-  }
- 
-  
-  
+updateeDisplay();
 
-};
